102a6199fb072a6de99a3f0a25bc02bc419cc9fa
dhruv-growexxer <dhruv.chauhan@growexx.com>
2022-03-20 17:07:44 +0530

    Style added

--- client/src/components/AddList.js
+++ client/src/components/AddList.js
@@ -1,35 +1,58 @@
-import React, { useState } from 'react'
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
+import { Button, Input, message } from "antd";
 
 const AddList = () => {
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("");
-    const [phone, setPhone] = useState(0);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState(0);
 
-    const addToList = async () => {
-        try {
-            const postList = await axios.post('http://localhost:5000/api/contact', {
-                first_name: firstName, last_name: lastName, phone
-            })
-            console.log(postList, "postList")
-        } catch (error) {
-            console.log("error ",error)
-        }
+  const addToList = async () => {
+    try {
+      const postList = await axios.post("http://localhost:5000/api/contact", {
+        first_name: firstName,
+        last_name: lastName,
+        phone,
+      });
+      console.log(postList, "postList");
+      message.success("Contact added, Refresh the page");
+    } catch (error) {
+      console.log("error ", error);
     }
+  };
   return (
-      
     <>
-        <label htmlFor="first_name">Enter first name: </label>  
-          <input type="text" name="first_name" id="" onChange={(e) => setFirstName(e.target.value)} />
-          
-        <label htmlFor="last_name">Enter last name: </label>  
-        <input type="text" name="last_name" id="" onChange={(e)=>setLastName(e.target.value)}/>
-        <label htmlFor="phone">Enter phone number: </label>  
-        <input type="number" name="phone" id="" onChange={(e) => setPhone(e.target.value)} />
-      <button onClick={addToList}>Add user</button>  <br /><br />
-      
+      <label htmlFor="first_name">Enter first name: </label>
+      <Input
+        style={{ width: 200, marginRight:10, marginLeft:10 }}
+        type="text"
+        name="first_name"
+        id=""
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <label htmlFor="last_name">Enter last name: </label>
+      <Input
+        style={{ width: 200, marginRight:10, marginLeft:10 }}
+        type="text"
+        name="last_name"
+        id=""
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <label htmlFor="phone">Enter phone number: </label>
+      <Input
+        style={{ width: 200, marginRight:2, marginLeft:2 }}
+        type="number"
+        name="phone"
+        id=""
+        onChange={(e) => setPhone(e.target.value)}
+      />
+      <Button className="margin-medium" type="primary" onClick={addToList}>
+        Add user
+      </Button>{" "}
+      <br />
+      <br />
     </>
-  )
-}
+  );
+};
 
-export default AddList
+export default AddList;
--- client/src/components/GetList.js
+++ client/src/components/GetList.js
@@ -1,101 +1,138 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { Modal, Button } from 'antd';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Modal, Button, Popconfirm, message, Input } from "antd";
 
 const GetList = () => {
-    const [contactList, setContactList] = useState([]);
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [newFirstName, setNewFirstName] = useState("")
-    const [newLastName, setNewLastName] = useState("");
-    const [newPhone, setNewPhone] = useState(0);
+  const [contactList, setContactList] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [newFirstName, setNewFirstName] = useState("");
+  const [newLastName, setNewLastName] = useState("");
+  const [newPhone, setNewPhone] = useState(0);
 
-    useEffect(() => {
-        try {
-            const fetchList = async () => {
-                const {data} = await axios.get('http://localhost:5000/api/contacts')
-                setContactList([...data])
-                console.log(data, "data")
-            }
-            fetchList();
-        } catch (error) {
-            console.log("error from GetList ", error)
-        } 
-    }, []);
-
-    console.log(contactList, "contactlist from outside of useEffect")
-
-    const showModal = () => {
-        setIsModalVisible(true);
-    };
-
-    const handleOk = (id, fname, lname, pno) => {
-        console.log(fname, lname, pno)
-        // if ('' !== newFirstName) {
-        //     console.log("newfirstname", newFirstName)
-        // }
-        // '' -> prevValue ie fname
-        // not'' -> newValue ie newFirstName
-        axios.put(`http://localhost:5000/api/contact/${id}`, {
-                id: id,
-                first_name: newFirstName === '' ? fname : newFirstName,
-                last_name: newLastName === '' ? lname : newLastName,
-                phone: newPhone === 0 ? pno : newPhone,
-            }
-        ).then(() => {
-            setIsModalVisible(false);
-        })
-        // setIsModalVisible(false)
-    };
-
-    const handleCancel = () => {
-        setNewFirstName('')
-        setNewLastName('')
-        setNewPhone(0)
-        setIsModalVisible(false);
-    };
-
-    const deleteContact = async (id) => {
-       await axios.delete(`http://localhost:5000/api/contact/${id}`)
+  useEffect(() => {
+    try {
+      const fetchList = async () => {
+        const { data } = await axios.get("http://localhost:5000/api/contacts");
+        setContactList([...data]);
+        console.log(data, "data");
+      };
+      fetchList();
+    } catch (error) {
+      console.log("error from GetList ", error);
     }
+  }, []);
 
-    return (
-        <>
-            {
-                contactList.map((contact, index) => {
-                
-                    return <div key={index}>
+  console.log(contactList, "contactlist from outside of useEffect");
 
-                        <label>First name: {' '}
-                            {contact.first_name}
-                        </label>{' '}
-                        <label>Last name: {' '}
-                            {contact.last_name}
-                        </label>{' '}
-                        <label>Phone: {' '}
-                            {contact.phone}
-                        </label>{' '}
-                        <br />
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
 
-                        <input type="text" name="new_first_name" placeholder='Edit first name' onChange={(e) => setNewFirstName(e.target.value)}/>
-                        <input type="text" name="new_last_name" placeholder='Edit last name' onChange={(e) => setNewLastName(e.target.value)}/>
-                        <input type="number" name="new_phone"  placeholder='Edit phone number' onChange={(e) => setNewPhone(e.target.value)}/>
-                        <button onClick={() =>  handleOk(contact._id, contact.first_name, contact.last_name, contact.phone)}>Update</button>
-                            
-                        {/* <Button type="primary" onClick={showModal}>
+  const handleOk = (id, fname, lname, pno) => {
+    console.log(fname, lname, pno);
+    // if ('' !== newFirstName) {
+    //     console.log("newfirstname", newFirstName)
+    // }
+    // '' -> prevValue ie fname
+    // not'' -> newValue ie newFirstName
+    axios
+      .put(`http://localhost:5000/api/contact/${id}`, {
+        id: id,
+        first_name: newFirstName === "" ? fname : newFirstName,
+        last_name: newLastName === "" ? lname : newLastName,
+        phone: newPhone === 0 ? pno : newPhone,
+      })
+      .then(() => {
+        setIsModalVisible(false);
+      });
+    // setIsModalVisible(false)
+  };
+
+  const handleCancel = () => {
+    setNewFirstName("");
+    setNewLastName("");
+    setNewPhone(0);
+    setIsModalVisible(false);
+  };
+
+  const deleteContact = async (id) => {
+    await axios.delete(`http://localhost:5000/api/contact/${id}`);
+    message.success("Contact Deleted, Refresh the page");
+  };
+
+  return (
+    <>
+      {contactList.map((contact, index) => {
+        return (
+          <div key={index}>
+            <label>First name: {contact.first_name}</label>{" "}
+            <label>Last name: {contact.last_name}</label>{" "}
+            <label>Phone: {contact.phone}</label> <br />
+            <div className="align-items">
+                    <Input
+                        style={{ width: 200, marginRight:10, marginLeft:10 }}
+                type="text"
+                name="new_first_name"
+                placeholder={contact.first_name}
+                onChange={(e) => setNewFirstName(e.target.value)}
+              />
+                    <Input
+                        style={{ width: 200, marginRight:10, marginLeft:10 }}
+                type="text"
+                name="new_last_name"
+                placeholder={contact.last_name}
+                onChange={(e) => setNewLastName(e.target.value)}
+              />
+                    <Input
+                        style={{ width: 200, marginRight:10, marginLeft:10 }}
+                type="number"
+                name="new_phone"
+                placeholder={contact.phone}
+                onChange={(e) => setNewPhone(e.target.value)}
+              />
+              <Button
+                className="margin-medium"
+                type="primary"
+                ghost
+                onClick={() =>
+                  handleOk(
+                    contact._id,
+                    contact.first_name,
+                    contact.last_name,
+                    contact.phone
+                  )
+                }
+              >
+                Update
+              </Button>
+
+              {/* <Button type="primary" onClick={showModal}>
                             Update
                         </Button>
                         <Modal title="Basic Modal" visible={isModalVisible} onOk={() =>  handleOk(contact._id, contact.first_name, contact.last_name, contact.phone)} onCancel={handleCancel}>
-                            <input type="text" name="new_first_name" placeholder='Edit first name' onChange={(e) => setNewFirstName(e.target.value)}/>
-                            <input type="text" name="new_last_name" placeholder='Edit last name' onChange={(e) => setNewLastName(e.target.value)}/>
-                            <input type="number" name="new_phone"  placeholder='Edit phone number' onChange={(e) => setNewPhone(e.target.value)}/>
+                            <Input type="text" name="new_first_name" placeholder='Edit first name' onChange={(e) => setNewFirstName(e.target.value)}/>
+                            <Input type="text" name="new_last_name" placeholder='Edit last name' onChange={(e) => setNewLastName(e.target.value)}/>
+                            <Input type="number" name="new_phone"  placeholder='Edit phone number' onChange={(e) => setNewPhone(e.target.value)}/>
                             
                         </Modal> */}
-                        <button onClick={()=>deleteContact(contact._id)}>Delete</button>
-                    </div>
-                })
-            }
-        </>
-  )
-}
+              {/* <Button type="primary" danger ghost onClick={()=>deleteContact(contact._id)}>Delete</Button> */}
 
-export default GetList
+              <Popconfirm
+                title="Are you sure delete this task?"
+                okText="Yes"
+                cancelText="No"
+                onConfirm={() => deleteContact(contact._id)}
+              >
+                <Button type="primary" danger ghost>
+                  Delete
+                </Button>
+              </Popconfirm>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default GetList;
